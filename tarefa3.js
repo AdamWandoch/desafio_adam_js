29a26d0295d942a1155efe8c65b2d95a12e0d506
AdamWandoch <wandoch.adam@gmail.com>
2022-10-08 23:00:49 -0300

    updated name of file and function

--- tarefa3.js
+++ tarefa3.js
@@ -1,7 +1,7 @@
 const numbers = [2, 7, 11, 15];
 const target = 22;
 
-const someFunction = () => {
+const solve = () => {
   for (let index1 = 0; index1 < numbers.length - 1; index1++) {
     for (let index2 = index1 + 1; index2 < numbers.length; index2++) {
       if (numbers[index1] + numbers[index2] === target) {
@@ -13,4 +13,4 @@ const someFunction = () => {
   console.log('No solution found.');
 };
 
-someFunction();
+solve();
