4f1c1c99af3a58814fdbbb6974dd76a086acc605
AdamWandoch <wandoch.adam@gmail.com>
2022-10-08 23:29:28 -0300

    updated readme

--- Readme.md
+++ Readme.md
@@ -1,1 +1,3 @@
 Solution in JS
+
+(Link to) => { [screenshot](https://drive.google.com/file/d/13NzC-zhYdWOMO8C6acjVE2GuJ7BFlzHu/view?usp=sharing) }
--- tarefa1.js
+++ tarefa1.js
@@ -2,7 +2,6 @@ const originalArray = [7, 5, 3, 9, 6, 4, 1];
 
 let newArray = originalArray.filter((number) => number !== 4);
 newArray = newArray.map((number) => (number === 9 ? 5 : number));
-
 let sum = newArray.reduce((a, b) => a + b, 0);
 
 console.log('originalArray: ' + originalArray);
